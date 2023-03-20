8bc1875cc39cbc052a8d5b3c8c8ea916c614901f
debalguha <debalguha.active@gmail.com>
2023-03-20 12:53:02 +1100

    Readme updated

--- user-service-api/src/main/java/com/asx/assignment/ums/rest/controller/ErrorHandlingControllerAdvice.java
+++ user-service-api/src/main/java/com/asx/assignment/ums/rest/controller/ErrorHandlingControllerAdvice.java
@@ -1,7 +1,9 @@
 package com.asx.assignment.ums.rest.controller;
 
 import com.asx.assignment.ums.rest.dto.ValidationErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class ErrorHandlingControllerAdvice {
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -38,16 +44,11 @@ public class ErrorHandlingControllerAdvice {
         return new ValidationErrorResponse(violations);
     }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    ValidationErrorResponse onHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        /*List<ValidationErrorResponse.Violation> violations = e..getFieldErrors()
-                .stream()
-                .map(fieldError -> new ValidationErrorResponse.Violation(fieldError.getField(), fieldError.getDefaultMessage()))
-                .collect(Collectors.toList());
-        ValidationErrorResponse error = new ValidationErrorResponse();*/
-        e.printStackTrace();;
-        return new ValidationErrorResponse(null);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handle(Exception ex, HttpServletRequest request) {
+        final String requestCorrelationId = Optional.ofNullable(request.getHeader("REQUEST_CORRELATION_ID")).orElse("");
+        log.error("Unexpected error when handling request with id {} ", requestCorrelationId, ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
+
 }
